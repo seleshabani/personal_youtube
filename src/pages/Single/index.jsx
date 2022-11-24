5e8b3800bbdd60521a6d1b6d14b4853e9e601472
seleshabani <seleshabani4@gmail.com>
2022-11-24 10:50:13 +0100

    commentaire : intégration du formualaire

--- src/pages/Single/index.jsx
+++ src/pages/Single/index.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Spinner } from "../../components/Spinner";
-import { ContainerWrapper } from "./styled";
+import { CommentsContainer, CommentsForm, CommentsFormContainer, CommentsFormProfil, ContainerWrapper } from "./styled";
 
 export const Single = ()=>{
     const {id} = useParams()
     const [video,setVideo] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const user = JSON.parse(localStorage.getItem('user'));
     useEffect(()=>{
         fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=AIzaSyAtyhesRrybOy-JDiv-rBfWxHpy90utvrA`)
         .then(response=>response.json())
@@ -19,11 +20,29 @@ export const Single = ()=>{
         .catch(error=>console.log(error))
     },[])
 
+    const handleForm = (e)=>{
+        e.preventDefault();
+    }
     return(
        <ContainerWrapper isLoading={isLoading}>
             {isLoading?<Spinner/>:<>
             <iframe src={`https://www.youtube.com/embed/${id}`}></iframe>
             <h1>{video[0].snippet.title}</h1>
+            <CommentsContainer>
+                <CommentsFormContainer onSubmit={handleForm}>
+                    <CommentsFormProfil>
+                    {user?<img src={user.profil_picture} alt="profil utilisateur" />:<i className="fa fa-user"></i>}
+                    </CommentsFormProfil>
+                    <CommentsForm>
+                        <div className="input_container">
+                            <input type={'text'} name='comment' placeholder="Ajoutez un commentaire"/>
+                        </div>
+                        <div className="btn_container">
+                            <button>Commenter</button>
+                        </div>
+                    </CommentsForm>
+                </CommentsFormContainer>
+            </CommentsContainer>
             </>}
         </ContainerWrapper>
     )
--- src/pages/Single/styled.jsx
+++ src/pages/Single/styled.jsx
@@ -17,3 +17,56 @@ export const ContainerWrapper = styled.div`
         font-family: 'Poppins';
     }
 `
+export const CommentsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-top: 30px;
+    margin-bottom: 30px;
+`
+export const CommentsFormContainer = styled.div`
+    width:60% ;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    align-content: center;
+`
+export const CommentsFormProfil = styled.div`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    overflow: hidden;
+    & img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
+export const CommentsForm = styled.form`
+    width: calc(100% - 100px);
+    & .input_container{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+        & input{
+            border: none;
+            border-bottom:solid 1px black;
+            width: 100%;
+        }
+    }
+    & .btn_container{
+        width: 100%;
+        display: flex;
+        justify-content: end;
+        & button{
+            cursor: pointer;
+        }
+    }
+`
